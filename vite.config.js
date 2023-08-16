cbbd7eead501df56371d9a8e81918ff9b94c5f9c
kyle goines <kyle.goines@gmail.com>
2023-08-16 10:05:44 -0400

    Init project

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+    root: 'src',
+    build: {
+        outDir: '../dist',
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+})
